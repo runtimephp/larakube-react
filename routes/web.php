d69119fde781edef11b85b40d470b61086918a28
Francisco Barrento <francisco.barrento@gmail.com>
2025-03-09 12:20:46 +0000

    chore(): removed ununsed job

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,12 @@
 
 declare(strict_types=1);
 
-use App\Jobs\CreateKubernetesClusterJob;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 require __DIR__.'/auth.php';
 
 Route::get('/', function () {
-    CreateKubernetesClusterJob::dispatch();
-
     return Inertia::render('welcome');
 })->name('home');
 
